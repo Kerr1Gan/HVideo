802ca41c43856b4e8ec2d12a2a8ac53010e63d6e
ethanxiang <674712964@qq.com>
2019-03-22 22:52:34 +0800

    H Video ui

--- app/src/main/java/com/ethan/and/MainActivity.java
+++ app/src/main/java/com/ethan/and/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ethan.and;
 
 import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
+import android.view.View;
 import android.view.Window;
 
+import com.common.componentes.TranslucentUtil;
+import com.common.utils.activity.ActivityUtil;
 import com.flybd.hvideo.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             getWindow().setExitTransition(new Explode());
         }
         super.onCreate(savedInstanceState);
+        TranslucentUtil.translucentWindow(this);
         setContentView(R.layout.activity_main);
+
+        View content = findViewById(R.id.content);
+        content.setPadding(content.getPaddingLeft(), content.getPaddingTop() + ActivityUtil.getStatusBarHeight(this), content.getPaddingRight(), content.getPaddingBottom());
+
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0);
+        drawerToggle.syncState();
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 }
